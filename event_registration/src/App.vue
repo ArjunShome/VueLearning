b8449c95aa3f3f16c9e7b8e8080c46c7d7782339
Arjun Shome <arjunshome111@gmail.com>
2025-01-20 09:55:43 +0530

    Added Vite Project

---
+++ event_registration/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <main class="container mx-auto my-8 space-y-8">
+    <h1 class="text-4xl">Event Registration App</h1>
+    <h2 class="text-2xl font-medium">All Events</h2>
+    <section class="grid grid-cols-2 gap-4">
+      <EventCard
+        v-for="i in 4"
+        :key="i"
+        title="Arjun's Practice"
+        when="February 2025"
+        description="Learning the wonderful view framework"
+        @register="console.log('Registered!!!')"
+      />
+    </section>
+  </main>
+</template>
+
+<script setup>
+import EventCard from '@/components/EventCard.vue';
+</script>
---
+++ event_registration/src/components/EventCard.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="border border-gray-900 bg-green-300 rounded-md">
+    <h3 class="p-2 font-medium text-xl border-b border-blue-600">{{ title }}</h3>
+    <p class="p-2 font-medium text-xl border-b border-blue-600">{{ when }}</p>
+    <p class="p-3">{{ description }}</p>
+    <section class="fles justify-end p-4">
+      <button
+        class="text-sm font-medium border border-gray-200 rounded-full px-3 py-1 hover:bg-blue-300"
+        @click="$emit('register')"
+      >
+        Register
+      </button>
+    </section>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  title: String,
+  when: String,
+  description: String
+});
+defineEmits(['register']);
+</script>
