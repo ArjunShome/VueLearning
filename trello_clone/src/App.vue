d8322eb4be1eebb0a47915717acf2b77c4b6829f
Arjun Shome <arjunshome111@gmail.com>
2025-11-14 19:54:44 +0530

    Initialize Vue 3 Vite Trello clone project
    Add initial project files for a Vue 3 app scaffolded with Vite, including configuration for ESLint, Prettier, EditorConfig, recommended VSCode extensions, and Tailwind CSS. Includes basic setup files, README, and starter source code.

---
+++ trello_clone/src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <main class="p-5 font-sans flex gap-3 overflow-x-auto">
+    <div class="bg-gray-100 p-3 rounded-lg min-w-[300px] flex-col">
+      <h2 class="mb-2 font-medium">Prioritized</h2>
+      <div class="bg-white p-2 my-2 rounded shadow cursor-pointer">
+        <span class="text-sm font-medium">Title Card</span>
+        <p class="text-gra-400 text-xs">Card Description</p>
+      </div>
+
+      <div class="bg-white p-2 my-2 rounded shadow cursor-pointer">
+        <span class="text-sm font-medium">Title Card</span>
+        <p class="text-gra-400 text-xs">Card Description</p>
+      </div>
+
+      <div class="bg-white p-2 my-2 rounded shadow cursor-pointer">
+        <span class="text-sm font-medium">Title Card</span>
+        <p class="text-gra-400 text-xs">Card Description</p>
+      </div>
+
+      <button
+        class="mt-2 w-full bg-transparent p-2 text-left text-sm font-medium text-gray-500 hover:bg-white"
+      >
+        + Add Card
+      </button>
+    </div>
+
+    <div class="bg-gray-100 p-3 rounded-lg min-w-[300px] flex-col">
+      <h2 class="mb-2 font-medium">In Progress</h2>
+      <div class="bg-white p-2 my-2 rounded shadow cursor-pointer">
+        <span class="text-sm font-medium">Title Card</span>
+        <p class="text-gra-400 text-xs">Card Description</p>
+      </div>
+
+      <div class="bg-white p-2 my-2 rounded shadow cursor-pointer">
+        <span class="text-sm font-medium">Title Card</span>
+        <p class="text-gra-400 text-xs">Card Description</p>
+      </div>
+
+      <div class="bg-white p-2 my-2 rounded shadow cursor-pointer">
+        <span class="text-sm font-medium">Title Card</span>
+        <p class="text-gra-400 text-xs">Card Description</p>
+      </div>
+
+      <button
+        class="mt-2 w-full bg-transparent p-2 text-left text-sm font-medium text-gray-500 hover:bg-white"
+      >
+        + Add Card
+      </button>
+    </div>
+  </main>
+</template>
